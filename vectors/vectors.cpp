a2ec4e9336d84dc4880f8364040582e0e22af7fd
travisTheRookie <travisb@fireflyint.com>
2021-03-15 20:55:10 -0400

    some edits to vectors.cpp

--- vectors/vectors.cpp
+++ vectors/vectors.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	// ===== declarations and initializations
+	// ====== declarations and initializations =================================
 	// declare a vector - empty
     vector<int> stuff;
     // decalre a vector with two slots, both initlized to 0
@@ -13,36 +13,40 @@ int main()
     // declare a vector and initilize it with 5 values
 	vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
 
-    // ==== output
+
+    // ====== output ===========================================================
     // cout << stuff[0] << "\n";  // causes: Segmentation fault (core dumped)
     cout << stuff1[0] << '\n';
 	cout << stuff1[1] << "\n";
 
 	for (int i = 0; i < vowels.size(); i++)
 	{
-		cout << vowels[i] << "\n";
+		cout << vowels[i] << " ";
 	}
+    cout << "\n";
 
 	vowels[2] = 'p';
 
 	for (int i = 0; i < vowels.size(); i++)
 	{
-		cout << vowels[i] << "\n";
+		cout << vowels[i] << " ";
 	}
+    cout << "\n";
 
 	vowels.push_back('y');
 	vowels[2] = 'i';
 
 	for (int i = 0; i < vowels.size(); i++)
 	{
-		cout << vowels[i] << "\n";
+		cout << vowels[i] << " ";
 	}
+    cout << "\n";
 
 	// ==== adding and removing elements
     stuff.push_back(69);
     stuff.push_back(42);
 
-    cout << stuff[1] << "\n";
+    cout << stuff[0] << " " << stuff[1] << "\n";
     cout << stuff.size() << "  ";  // prints 2
     stuff.pop_back(); // removes 42
     cout << stuff.size() << "\n";  // prints 1
