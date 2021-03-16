2144e709a24028aeacb3477e65c960153bd7bade
travisTheRookie <travisb@fireflyint.com>
2021-03-15 22:20:55 -0400

    Added the User class

--- login_system/login.cpp
+++ login_system/login.cpp
@@ -2,11 +2,45 @@
 #include <fstream>
 #include <string>
 
-
 using namespace std;
+
+
+class User 
+{
+    private:
+        string username,
+               password;
+
+    public:
+        string getName()
+        {
+        	return username; 
+        }
+        void setName(string name)
+        {
+            username = name;
+        }
+        string getPassword()
+        {
+        	return password;
+        }
+        void setPassword(string passwrd)
+        {
+            password = passwrd;
+        }
+};
+
 
 int main()
 {
-	cout << "Hello World\n";
+    User user;
+
+    user.setName("George");
+    cout << "Name:     " << user.getName() << "\n";
+    user.setPassword("kajfh6dd");
+    cout << "Password: " << user.getPassword() << "\n";
+
+
 	return 0;
 }
+
