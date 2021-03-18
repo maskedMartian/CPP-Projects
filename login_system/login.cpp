d4182494e4a7c62803d81258ea69dcbdaaf3146e
travisTheRookie <travisb@fireflyint.com>
2021-03-17 20:54:28 -0400

    Added and tested  the FileManager class

--- login_system/login.cpp
+++ login_system/login.cpp
@@ -15,6 +15,16 @@ int main()
     user.setPassword("kajfh6dd");
     cout << "Password: " << user.getPassword() << "\n";
 
+    FileManager fileManager(user.getName());
+
+    if (!fileManager.fileExists())
+    {
+    	fileManager.saveToFile(user.getName(), user.getPassword());
+    }
+    if (fileManager.fileExists())
+    {
+    	cout << "The password is: " << fileManager.readFromFile(user.getName()) << "\n";
+    }
 
 	return 0;
 }
--- login_system/myClasses.cpp
+++ login_system/myClasses.cpp
@@ -1,6 +1,7 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -17,6 +18,47 @@ class User
         void setName(string name);
         string getPassword();
         void setPassword(string passwrd);
+};
+
+
+class FileManager
+{
+    private:
+        string filename;
+    public:
+        FileManager(string username)
+        {
+            filename = username + ".txt";
+        }
+        void saveToFile(string username, string password)
+        {
+            ofstream saveFile(filename);
+
+            saveFile << password;
+            saveFile.close();
+        }
+        string readFromFile(string username)
+        {
+            string password;
+
+            ifstream readFile(filename);
+            getline(readFile, password);
+            readFile.close(); 
+            return password;
+        }
+        bool fileExists()
+        {
+            ifstream file(filename);
+
+            if(!file)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
 };
 
 
