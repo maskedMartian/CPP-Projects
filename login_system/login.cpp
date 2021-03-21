84915c6b3de3c68549e43dbaf04bb311d88a8772
travisTheRookie <travisb@fireflyint.com>
2021-03-21 19:18:45 -0400

    Move user registration and login logic to BusinessManager class

--- login_system/login.cpp
+++ login_system/login.cpp
@@ -11,60 +11,22 @@ const int EXIT = 3;
 
 int main()
 {
-    int menuChoice;
+    BusinessManager businessManager;
 
-	User user;
-	FileManager fileManager;
-	UserInterface userInterface;
-
-    menuChoice = userInterface.inputMenuChoice();
-
-    while (menuChoice != EXIT)
+    businessManager.setMenuSelection();
+    while (businessManager.getMenuSelection() != EXIT)
     {
-    	if (menuChoice == REGISTER)
-    	{
-            user.setName(userInterface.getUsername());
-            fileManager.setFilename(user.getName());
-            if (!fileManager.fileExists())
-            {
-                user.setPassword(userInterface.getPassword());
-                while (!userInterface.verifyPassword(user.getPassword()))
-                {
-                	cout << "\nERROR: Passwords did not match. Try again.\n\n";
-                    user.setPassword(userInterface.getPassword());	
-                }
-                fileManager.setFilename(user.getName());
-                fileManager.saveToFile(user.getPassword());
-                cout << "\nSUCCESS: User registered\n";
-            }
-            else
-            {
-                cout << "\nERROR: User already registered.\n";
-            }
-    	}
-    	else if (menuChoice == LOGIN)
-    	{
-            user.setName(userInterface.getUsername());
-            fileManager.setFilename(user.getName());
-            if (!fileManager.fileExists())
-            {
-                cout << "\nERROR: Username not registered\n";
-            }
-            else
-            {
-                user.setPassword(userInterface.getPassword());
-                if (fileManager.readFromFile() != user.getPassword())
-            	{
-                    cout << "\nERROR: Incorrect Password\n\n";
-            	}
-            	else
-            	{
-            	    cout << "\nSUCCESS: You are logged in\n";
-            	}	
-            }
-    	}
-        menuChoice = userInterface.inputMenuChoice();
+        if (businessManager.getMenuSelection() == REGISTER)
+        {
+            businessManager.registerUser();
+        }
+        else if (businessManager.getMenuSelection() == LOGIN)
+        {
+            businessManager.loginUser();
+        }
+        businessManager.setMenuSelection();
     }
+    
 	return 0;
 }
 
--- login_system/myClasses.cpp
+++ login_system/myClasses.cpp
@@ -46,6 +46,68 @@ class UserInterface
 };
 
 
+class BusinessManager
+{
+    private:
+        int menuSelection;
+        User user;
+        FileManager fileManager;
+        UserInterface userInterface;
+
+    public:
+        void setMenuSelection()
+        {
+            menuSelection = userInterface.inputMenuChoice();
+        }
+        int getMenuSelection()
+        {
+            return menuSelection;
+        }
+        void registerUser()
+        {
+            user.setName(userInterface.getUsername());
+            fileManager.setFilename(user.getName());
+            if (!fileManager.fileExists())
+            {
+                user.setPassword(userInterface.getPassword());
+                while (!userInterface.verifyPassword(user.getPassword()))
+                {
+                    cout << "\nERROR: Passwords did not match. Try again.\n\n";
+                    user.setPassword(userInterface.getPassword());  
+                }
+                fileManager.setFilename(user.getName());
+                fileManager.saveToFile(user.getPassword());
+                cout << "\nUser registered\n";
+            }
+            else
+            {
+                cout << "\nERROR: User already registered.\n";
+            }
+        }
+        void loginUser()
+        {
+            user.setName(userInterface.getUsername());
+            fileManager.setFilename(user.getName());
+            if (!fileManager.fileExists())
+            {
+                cout << "\nERROR: User not registered\n";
+            }
+            else
+            {
+                user.setPassword(userInterface.getPassword());
+                if (fileManager.readFromFile() != user.getPassword())
+                {
+                    cout << "\nERROR: Incorrect Password\n";
+                }
+                else
+                {
+                    cout << "\nYou are logged in\n";
+                }   
+            }
+        }
+};
+
+
 // Class method definitions
 string User::getName()
 {
